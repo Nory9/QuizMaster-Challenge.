14483a8795c7d7d787b788411102177fd012a1aa
Nory9 <noormisk94@gmail.com>
2024-06-09 16:15:08 +0300

    quiz

--- quizmaster/Program.cs
+++ quizmaster/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            try
+            {
+                string[] ss = questions();
+                string[] aa = answers();
+                int c = StartQuiz.quiz(ss, aa);
+                Console.WriteLine($"your mark is {c} out of 10");
+                if (c < 5)
+                {
+                    Console.WriteLine("dont feel upset you can alwayes try again ;)");
+                }
+                else
+                {
+                    Console.WriteLine(" Great job! keep it up");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong please try again!");
+            }
+            finally {
+                Console.WriteLine("thank you for using and supporting QuizMaster :)");
+            }
+          
+        }
+        /*for (int i = 0;i< ss.Length;i++) {
+                Console.WriteLine(ss[i]);
+                Console.WriteLine();
+                Console.WriteLine(aa[i]);
+            }*/
+        static string[] questions()
+        {
+            string[] q = { "1. C# is a programming language, developed by ___.\nA. Oracle\r\nB. Microsoft\r\nC. GNU project\r\nD. Google", "2. C# runs on the ___.\r\n\r\nA. .NET Framework\r\nB. Java Virtual Machine\r\nC. Both A. and B.\r\nD. None of the above", "3. Is C# an object-oriented programming language?\r\n\r\nA. Yes\r\nB. No", "4. What is CLR in C#?\r\n\r\nA. It is a virtual machine component of Microsoft .NET Framework\r\nB. It is a virtual machine component of JVM\r\nC. It is a compiler to compiler the C# code\r\nD. All of the above", "5. Is C# programming language a case-sensitive?\r\n\r\nA. Yes\r\nB. No", "6.What is the correct syntax to declare a variable in C#?\r\n\r\nA. type variableName = value;\r\nB. type variableName;\r\nC. variableName as type = value;\r\nD. Both A. and B.", "7.Which C# keyword is used to define a constant?\r\n\r\nA. define\r\nB. fixed\r\nC. constant\r\nD. const", "8.Which is not a valid C# data type?\r\n\r\nA. long\r\nB. int\r\nC. float\r\nD. complex", "9. What is 'Console' in C#?\r\n\r\nA. Class\r\nB. Object\r\nC. Method\r\nD. Structure", "10.Which is/are the correct method(s) to input a float value in C#?\r\n\r\nA. Parse(Console.ReadLine())\r\nB. ToSingle(Console.ReadLine())\r\nC. ToFloat(Console.ReadLine());\r\nD. Both A and B" };
+            return q;
+        }
+        static string[] answers() {
+            string[] s = { "B", "A","A","A","A","D","D","D","A","B" };
+            return s;
         }
     }
 }
---
+++ quizmaster/StartQuiz.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quizmaster
+{
+    public class StartQuiz
+    {
+        public static int quiz(string[] questions, string[] answers) {
+            int count = 0,i=0;
+            Console.WriteLine("Wellcome to QuizMaster\n this quiz is about C# basics it will have 10 questions so get ready and let get started!");
+            Console.WriteLine("but before we start please make sure to enter A/a ,B/b ,C/c or D/d as an answer");
+            while (i != 9) {
+               
+                Console.WriteLine(questions[i]);
+                string ans=Console.ReadLine();
+                if (ans == answers[i] || ans.ToUpper() == answers[i])
+                {
+                    count++;
+                    Console.WriteLine("Good Job you've got this right!");
+
+                }
+                else if (ans != "A" && ans != "B" && ans != "C" && ans != "D" && ans != "a" && ans != "b" && ans != "c" && ans != "d") { 
+                Console.WriteLine("you entered a not suppoted format! please try again making sure your anwser is A/a B/b C/c D/d \n ");
+                    Console.WriteLine(questions[i]);
+                    string a = Console.ReadLine();
+                    if (a == answers[i] || a.ToUpper() == answers[i])
+                    {
+                        count++;
+                        Console.WriteLine("Good Job you've got this right!");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("you've got this wrong again ;(");
+                    }
+                }
+                
+               
+                else {
+                    Console.WriteLine("your answer is wrong :( but dont worry you have one more shoot! lets try again");
+                    Console.WriteLine(questions[i]);
+                    string bb = Console.ReadLine();
+                    if (bb == answers[i] || bb.ToUpper() == answers[i])
+                    {
+                        count++;
+                        Console.WriteLine("Good Job you've got this right!");
+
+                    }
+                    else {
+                        Console.WriteLine("you've got this wrong again ;(");
+                    }
+
+                   
+                }
+
+                i++;
+            }
+
+        return count;
+        }
+    }
+}
